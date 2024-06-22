e81a6f2f9e981626f9227ac65847f1e4f8e148e3
hadi499 <hadi.ygcl@gmail.com>
2024-06-22 19:09:59 +0700

    up

---
+++ src/components/Balance.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <h4>Your Balance</h4>
+
+  <h1 v-if="total < 0" id="balance">- ${{ Math.abs(total) }}</h1>
+
+  <h1 v-else id="balance">+ ${{ total }}</h1>
+</template>
